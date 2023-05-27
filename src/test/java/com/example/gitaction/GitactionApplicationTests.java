c18bfae253f81ccf8674784040fd9c74cc43e716
doi-h <jeonghwanlee15@gmail.com>
2023-05-27 18:58:41 +0900

    git action test

---
+++ src/test/java/com/example/gitaction/GitactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
