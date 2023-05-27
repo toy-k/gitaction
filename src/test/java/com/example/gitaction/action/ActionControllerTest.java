ff052b21cf4f8f6b32b1b9c86897fc59396b51a2
doi-h <jeonghwanlee15@gmail.com>
2023-05-27 17:50:47 +0900

    git action test

--- src/test/java/com/example/gitaction/action/ActionControllerTest.java
+++ src/test/java/com/example/gitaction/action/ActionControllerTest.java
@@ -1,19 +1,15 @@
 package com.example.gitaction.action;
 
-import com.example.gitaction.GitactionApplication;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 //@SpringBootTest(classes = GitactionApplication.class)
-
 @WebMvcTest(ActionController.class)
 @DisplayName("ActionControllerTest")
 class ActionControllerTest {
