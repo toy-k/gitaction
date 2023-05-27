122c5b98fa8491aa7704c0ab8bae47ea3382ad19
doi-h <jeonghwanlee15@gmail.com>
2023-05-27 17:04:05 +0900

    git action test

--- src/test/java/com/example/gitaction/ActionControllerTest.java
+++ src/test/java/com/example/gitaction/ActionControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = GitactionApplication.class)
+@SpringBootTest(classes = {GitactionApplication.class})
 @DisplayName("ActionControllerTest")
 class ActionControllerTest {
 
