cdbf098e5c513e19f77bdd79170b1cd905e3b9d2
doi-h <jeonghwanlee15@gmail.com>
2023-05-27 16:59:04 +0900

    git action test

--- src/test/java/com/example/gitaction/ActionControllerTest.java
+++ src/test/java/com/example/gitaction/ActionControllerTest.java
@@ -1,6 +1,7 @@
-package com.example.gitaction.action;
+package com.example.gitaction;
 
 import com.example.gitaction.GitactionApplication;
+import com.example.gitaction.action.ActionController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
